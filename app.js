eb516ece04145450536857636c3827da9772383f
Abhishek <ay143488@gmail.com>
2023-01-02 21:57:27 +0530

    ho gya

--- app.js
+++ app.js
@@ -124,7 +124,7 @@ app.get("/:customListName", function(req,res){
 
 
 let port = process.env.PORT;
-if(port == NULL||port == ""){
+if(port == null||port == ""){
     port = 3000;
 }
 app.listen(port,function(){
